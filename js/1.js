8fcb0439c7a754ac8cb39067b80974d9ca66ed3d
cianly <2415940209@qq.com>
2017-12-25 17:37:35 +0800

    简单实例

---
+++ js/1.js
@@ -0,0 +1,17 @@
+
+
+    var myApp = angular.module('spicyApp1', []);
+     
+    myApp.controller('SpicyCtrl', ['$scope', function($scope){
+        $scope.spice = 'very';
+        
+        $scope.chiliSpicy = function() {
+            $scope.spice = 'chili';
+        };
+        
+        $scope.jalapenoSpicy = function() {
+            $scope.spice = 'jalapeño';
+        };
+    }]);
+
+
